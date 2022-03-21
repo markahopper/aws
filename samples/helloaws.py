7dee6cc1a33d4be067c7e82d8985090ad2268a98
EC2 Default User <ec2-user@ip-172-31-11-70.us-west-2.compute.internal>
2022-03-21 17:04:30 +0000

    account change

--- samples/helloaws.py
+++ samples/helloaws.py
@@ -13,7 +13,7 @@ for bucket in s3.buckets.all():
 print(buffer)
 
 # Publish the bucket names to SNS topic
-topic = sns.Topic("arn:aws:sns:us-west-2:691995883950:DevTopic")
+topic = sns.Topic("arn:aws:sns:us-west-2:226282727140:DevTopic")
 response = topic.publish(
         Message="My buckets: " + buffer
     )
